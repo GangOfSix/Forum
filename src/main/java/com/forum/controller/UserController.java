5faea517beaa08806287f60cd00d3dba0c3c3348
kakeru00 <495263078@qq.com>
2016-04-24 17:22:21 +0800

    update

--- src/main/java/com/forum/controller/UserController.java
+++ src/main/java/com/forum/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.forum.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,14 +20,55 @@ public class UserController {
 	private UserService userService;
 	Logger logger = Logger.getLogger(UserController.class);
 	
-	@RequestMapping(value="/userRegister",method = RequestMethod.POST)
+	@RequestMapping(value="/register",method = RequestMethod.POST)
 	@ResponseBody 
-	public String userRegister(User user)  {		
+	public String register(User user)  {		
 		logger.info(user.getNickname());
 		
 		if(userService.save(user)!=null)
 			return "success";
 		else return "fail";
+	}
+	
+	@RequestMapping(value="/get")
+	@ResponseBody 
+	public User getById(String id){	
+		return userService.getById(id);
+	}
+	
+	
+	public User loginWay(String str){
+	
+		if(str.matches("\\d{11}"))
+			return  userService.getByPhonenumber(str);
+		else if(str.matches("^[a-zA-Z\u4e00-\u9fa5][a-zA-Z0-9_\u4e00-\u9fa5]+$"))		
+			return userService.getByNickname(str);
+		else if(str.matches("^[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_]+\\.\\w+"))
+			return  userService.getByEmail(str);
+		else return null;
+	}
+	
+	@RequestMapping(value="/login")
+	@ResponseBody 
+	public boolean login(String str,String password,HttpSession session){
+		logger.info("²ÎÊý´«µÝ"+str+password);
+		User user = this.loginWay(str);
+		
+		if((user!=null)&&password.equals(user.getPassword())){
+			logger.info(user.getNickname());
+			session.setAttribute("user", user);
+			return true;
+		}
+		else return false;
+	}
+	
+	
+	
+	@RequestMapping(value="/validate")
+	@ResponseBody 
+	public boolean validateNickname(String nickname){
+		
+		return (userService.getByNickname(nickname)==null);
 	}
 /*	@RequestMapping("/get")
 	@ResponseBody
--- src/main/java/com/forum/service/UserService.java
+++ src/main/java/com/forum/service/UserService.java
@@ -22,4 +22,25 @@ public class UserService {
 	public List<User> find() {
 		return userDao.find("from User");
 	}
+	
+	public User getByNickname(String nickname) {
+		String[] param = {nickname};
+		return userDao.get("from User where nickname = ?", param);
+	}
+
+	public User getById(String id) {
+		return userDao.get(User.class, id);
+	}
+
+	public User getByPhonenumber(String phoneNumber) {
+		
+		Object[] param = {Long.parseLong(phoneNumber)};
+		return userDao.get("from User where phonenumber = ?", param);
+		
+	}
+	public User getByEmail(String email) {
+		String[] param = {email};
+		return userDao.get("from User where email = ?", param);
+		
+	}
 }
